29a339379c6c9a204fdd90dc405f40180cb2fb06
dniprom <zenaz3909@gmail.com>
2022-08-16 14:32:42 +0300

    і

--- UA/src/game.js
+++ UA/src/game.js
@@ -202,7 +202,7 @@ var Game = Game || (function (createjs, $) {
 
             //add the background
             var canvasBackground = new createjs.Shape();
-            canvasBackground.graphics.setStrokeStyle(1).beginStroke("#F8F3F3").beginFill("#80C4FF");
+            canvasBackground.graphics.setStrokeStyle(1).beginStroke("#F8F3F3").beginFill("#FEE1C4");
             canvasBackground.graphics.drawRect(0, 0, 1280, 720);
             stage.addChild(canvasBackground);
 
